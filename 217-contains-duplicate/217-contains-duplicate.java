b301cf5199689fe67882e26703e29e42d716e048
SumitGupta0209 <53830900+SumitGupta0209@users.noreply.github.com>
2022-04-04 18:28:16 +0530

    Update 217-contains-duplicate.java

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -15,3 +15,26 @@ class Solution {
         return false;
     }
 }
+//more optimised soln
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int sum=0;
+        int maxSum=nums[0];
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+            if(sum>maxSum)
+            {
+                maxSum=sum;
+            }
+            if(sum<0)
+            {
+                sum=0;
+            }
+        }
+        return maxSum;
+    }
+    
+}
