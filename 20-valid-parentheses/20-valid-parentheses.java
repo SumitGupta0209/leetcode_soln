8cac87475be57693f45fcd220f2cb48276bb5d3e
SumitGupta0209 <53830900+SumitGupta0209@users.noreply.github.com>
2022-05-02 20:43:56 +0530

    Time: 1 ms (99.41%), Space: 40.3 MB (83.71%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        // Create a stack to hold opening brackets.
+        Stack<Character> stack = new Stack<Character>();
+        
+        // Loop over characters of string {s}
+        for(int i=0; i<s.length(); i++) {
+            
+            char curr = s.charAt(i);
+            
+            // If current character {curr} is opening bracket, add it to {stack} 
+            if( curr == '(' || curr == '{' || curr == '[' ) {
+                stack.push(curr);
+            } else if( stack.isEmpty() ) {
+                // Else current character is closing bracket, If stack is empty, return false.
+                return false;
+            } else {
+                // Else stack contains opening brackets, match it with the current closing bracket. If not a valid match return false.
+                char last = stack.pop();
+                
+                if( 
+                    (last == '(' && curr != ')') ||
+                    (last == '{' && curr != '}') ||
+                    (last == '[' && curr != ']') 
+                ) {
+                    return false;
+                }
+            }
+        }
+        
+        // If stack is empty, string is valid.
+        return stack.isEmpty();
+    }
+}
