5f69e53eb307eead3c3853f4fbd5febf471f81d8
SumitGupta0209 <53830900+SumitGupta0209@users.noreply.github.com>
2022-05-08 21:07:52 +0530

    Time: 5 ms (15.16%), Space: 39.7 MB (73.59%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        boolean[] dp = new boolean[10001];
+        for(int i=2 ; i<=n ; i++){          //start with 2 as n=1 is false anyways
+            for(int j=1 ; j<i ; j++){
+                if(i%j == 0 && !dp[i-j]){     // to check the factors of i and if i-j leads to an odd number to 
+                    dp[i] = true;             //make sure that bob get odd i.e, dp[i-j] == false (a false is an odd 
+                    break;                    //number accoding to our solution)
+                }                             //if dp[i] is not set to true, indicates that it is an odd number and 
+            }                                 //alice looses
+        }
+        return dp[n];
+    }
+}
