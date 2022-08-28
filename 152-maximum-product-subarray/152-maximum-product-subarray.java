02023ff7af9518bdf1542ba0cc143510e3acd7f3
SumitGupta0209 <53830900+SumitGupta0209@users.noreply.github.com>
2022-08-28 14:55:42 +0530

    Time: 2 ms (78.25%), Space: 45.1 MB (36.10%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -2,29 +2,29 @@ class Solution {
     public int maxProduct(int[] nums) {
         
         int maxsum = nums[0];
-        int cursum = 1;
-        if(nums.length==1){
-        return nums[0];
-        }
-        for(int i= 0;i<nums.length;i++){
-        cursum = cursum*nums[i];
+    int cursum = 1;
+    if(nums.length==1){
+    return nums[0];
+    }
+    for(int i= 0;i<nums.length;i++){
+    cursum = cursum*nums[i];
 
-                if(cursum>maxsum){
-                    maxsum=cursum;
-                }
-                if(cursum==0){
-                    cursum = 1;
-                }
-            }cursum= 1;
-            for(int i= nums.length-1;i>0;i--){
-                cursum = cursum*nums[i];
+            if(cursum>maxsum){
+                maxsum=cursum;
+            }
+            if(cursum==0){
+                cursum = 1;
+            }
+        }cursum= 1;
+        for(int i= nums.length-1;i>0;i--){
+            cursum = cursum*nums[i];
 
-                if(cursum>maxsum){
-                    maxsum=cursum;
-                }
-                if(cursum==0){
-                    cursum = 1;
-                }
+            if(cursum>maxsum){
+                maxsum=cursum;
+            }
+            if(cursum==0){
+                cursum = 1;
+            }
     }return maxsum;
 
         
